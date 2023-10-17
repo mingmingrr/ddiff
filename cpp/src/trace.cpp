4cf8866e3520696ffae750bf818d7eb155431b27
mingmingrr <12855406+mingmingrr@users.noreply.github.com>
2023-10-17 11:23:00 -0400

    cpp: remove boost json

--- cpp/src/trace.cpp
+++ cpp/src/trace.cpp
@@ -1,6 +1,8 @@
 #include "trace.hpp"
 
-#include <boost/json.hpp>
+#include <cctype>
+#include <sstream>
+#include <iomanip>
 
 struct time now;
 
@@ -20,8 +22,18 @@ void tracef(std::ostream& out) {
 	out << std::endl;
 }
 
+std::string serialize(std::string str) {
+	std::ostringstream out;
+	for(char chr : str)
+		if(std::isprint(chr))
+			out << chr;
+		else
+			out << "\\x" << std::hex << std::setw(2) << (int)chr;
+	return out.str();
+}
+
 std::ostream& operator <<(std::ostream& out, ftxui::Event event) {
-	out << "Event: " << boost::json::serialize(event.input()) << " -> ";
+	out << "Event: " << serialize(event.input()) << " -> ";
 	if (event.is_character()) {
 		out << "Event::Character(\"" << event.character() << "\")";
 	} else if (event.is_mouse()) {
